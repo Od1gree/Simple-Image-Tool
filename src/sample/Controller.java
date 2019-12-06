9918f4f963f3fe65bc361d90d7906fabb20fb7b1
joe941104 <joe941104@gmail.com>
2019-12-05 20:59:17 -0800

    Creat the load image button and implement the function of showing image's information

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,24 +1,35 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import models.ImageFrame;
 
-import java.awt.event.MouseEvent;
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.TreeMap;
 
 public class Controller{
     @FXML
     private Button hlo;
+
+    @FXML
+    private VBox big_img;
+
+    @FXML
+    private ListView<String> Image_Info;
 
     @FXML
     private VBox hlobox;
@@ -26,10 +37,19 @@ public class Controller{
     @FXML
     private Label hlolabel;
 
+    @FXML
+    private Button delateImage;
+
+    ArrayList<ImageView> imgList = new ArrayList<>();
+
 
     private int i=0;
 
-    private  String pass;
+    //private  String pass;
+
+    private ImageFrame imageFrame =new ImageFrame();
+
+    ImageView currentImg;
 
 
 //    Controller(){
@@ -47,10 +67,37 @@ public class Controller{
         if (file != null) {
 
             System.out.println(file.getAbsolutePath());
-            pass=file.getAbsolutePath();
+            //pass=file.getAbsolutePath();
             //File file = new File("/System/Library/CoreServices/loginwindow.app/Contents/Resources/LogOut.png");
             Image image = new Image(file.toURI().toString());
             ImageView iv = new ImageView(image);
+            imgList.add(iv);
+            System.out.println(hlo.getParent());
+            models.Image img = new models.Image(file.getAbsolutePath());
+            imageFrame.append(img);
+            iv.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+                    System.out.println("123");
+                    big_img.getChildren().clear();
+                    ImageView vi = new ImageView((image));
+
+
+                    big_img.getChildren().add(vi);
+
+                    vi.setFitHeight(300);
+                    vi.setPreserveRatio(true);
+                    currentImg = iv;
+
+                    Image_Info.getItems().clear();
+                    TreeMap<String,String> exif = img.getAttributes();
+                    exif.forEach((k,v)->{
+                        Image_Info.getItems().add(k+":"+v);
+                    });
+                    System.out.print(exif);
+
+                }
+            });
             iv.setFitHeight(100);
             iv.setFitWidth(100);
             hlobox.getChildren().add(iv);
@@ -62,10 +109,19 @@ public class Controller{
 
     @FXML
     void clk(KeyEvent event) {
-        System.out.println(pass);
+        //System.out.println(pass);
     }
 
 
+
+    @FXML
+    void delImage(ActionEvent event) {
+        imgList.remove(currentImg);
+        hlobox.getChildren().remove(currentImg);
+        big_img.getChildren().clear();
+        Image_Info.getItems().clear();
+
+    }
 
 
 }
--- src/test/entrance.java
+++ src/test/entrance.java
@@ -6,23 +6,23 @@ import java.util.TreeMap;
 public class entrance {
     public static void main(String[] args){
 
-        String path = "/Users/mac/Downloads/test_set/16707ff998ed6d087036d4f44b1296ef96e1b1cf.png";
-
-        Image img = new Image(path);
-        ImageFrame imageFrame = new ImageFrame();
-        imageFrame.append(img);
-
-
-        String path1 = "/Users/mac/Downloads/test_set/DSC_1621.JPG";
-        Image img1 = new Image(path1);
-        imageFrame.append(img1);
-
-        Filter redFilter = new RGBFilter(123);
-        ((RGBFilter)redFilter).setFilter(0, -100, 0, -100);
-        boolean[] choiceList = {true, true};
-        imageFrame.addFilter(redFilter);
-        ImageFrame newFrame = imageFrame.edit(choiceList, redFilter.getId(), "jpg");
-        newFrame.addFilter(redFilter);
-        newFrame.save(choiceList,"/Users/mac/Downloads/test_set/ttt/");
+//        String path = "/Users/mac/Downloads/test_set/16707ff998ed6d087036d4f44b1296ef96e1b1cf.png";
+//
+//        Image img = new Image(path);
+//        ImageFrame imageFrame = new ImageFrame();
+//        imageFrame.append(img);
+//
+//
+//        String path1 = "/Users/mac/Downloads/test_set/DSC_1621.JPG";
+//        Image img1 = new Image(path1);
+//        imageFrame.append(img1);
+//
+//        Filter redFilter = new RGBFilter(123);
+//        ((RGBFilter)redFilter).setFilter(0, -100, 0, -100);
+//        boolean[] choiceList = {true, true};
+//        imageFrame.addFilter(redFilter);
+//        ImageFrame newFrame = imageFrame.edit(choiceList, redFilter.getId(), "jpg");
+//        newFrame.addFilter(redFilter);
+//        newFrame.save(choiceList,"/Users/mac/Downloads/test_set/ttt/");
     }
 }
